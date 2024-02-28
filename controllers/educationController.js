ef894dc6192a29f9bba30fc2c249a645b671c501
iekeng <ekeng7.eb@gmail.com>
2024-02-28 12:52:30 +0100

    update education feature

--- controllers/educationController.js
+++ controllers/educationController.js
@@ -17,6 +17,31 @@ exports.createEducation = async (req, res) => {
     }
 };
 
+exports.updateEducation = async (req, res) => {
+    const {userId, educationId} = req.params;
+    const {institution, startDate, endDate, degree, location, course} = req.body;
+    const education = {}
+
+    try {
+        educationToUpdate = await Education.findById(educationId);
+
+        if (educationToUpdate.user === userId) {
+            education.institution = institution || educationToUpdate.institution;
+            education.startDate = startDate || educationToUpdate.startDate;
+            education.endDate = endDate || educationToUpdate.endDate
+            education.degree = degree || educationToUpdate.degree;
+            education.location = location || educationToUpdate.location;
+            education.course = course || educationToUpdate.course;
+        }
+
+        const updatedEducation = Experience.findByIdAndUpdate(educationId, education, {new: true})
+        res.status(200).json(updatedEducation);
+
+    } catch {
+        res.status(500).json({error: 'An error occured while creating the education record'})
+    }
+};
+
 exports.getEducationById = async(req, res) => {
     try {
         const userId = req.params.param1;
--- routes/education.js
+++ routes/education.js
@@ -3,9 +3,8 @@ const router =  express.Router();
 const educationController = require('../controllers/educationController');
 
 router.post('/:userId', educationController.createEducation);
-
 router.get('/:userId/:educationId', educationController.getEducationById);
-
+router.put('/:userId/:educationId', educationController.updateEducation);
 router.get('/:userId', educationController.getAllEducation);
 
 module.exports = router;
