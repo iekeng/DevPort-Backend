4332ae12da1e387acfeaddf8a6d7ba81fe018d8f
iekeng <ekeng7.eb@gmail.com>
2024-02-28 12:41:22 +0100

    updateEcperince controller

--- controllers/experienceController.js
+++ controllers/experienceController.js
@@ -3,13 +3,14 @@ const Experience = require('../models/Experience')
 exports.createExperience = async (req, res) => {
     try {
         const userId = req.params.userId;
-        const experienceData = req.body;
-        const experience = new Experience(experienceData);
+        const experienceArray = req.body;
+        for (const experience of experienceArray){
+            const newExperience = new Experience(experience);
+            experience.user = userId;
 
-        experience.user = userId;
-
-        await experience.save();
-
+            await experience.save();
+        }
+        
         res.status(201).json(experience);
 
     } catch {
@@ -19,8 +20,7 @@ exports.createExperience = async (req, res) => {
 
 exports.getExperienceById = async(req, res) => {
     try {
-        const userId = req.params.param1;
-        const experienceId = req.params.param2;
+        const {userId, experienceId} = req.params;
         const experience = await Experience.find({_id: experienceId, user: userId})
         res.status(200).json(experience);
     }catch {
@@ -30,7 +30,7 @@ exports.getExperienceById = async(req, res) => {
 
 exports.getAllExperience = async(req, res) => {
     try{
-        const userId = req.params.userId;
+        const {userId} = req.params;
 
         const experience = await Experience.find({user: userId}).exec();
 
@@ -42,30 +42,31 @@ exports.getAllExperience = async(req, res) => {
 
 exports.updateExperience = async(req, res) => {
     const {userId, experienceId} = req.params;
-    const {organisation, position, startDate, endDate, achievements, responsibilities, user} = req.body;
+    const {organisation, position, startDate, endDate, achievements, responsibilities} = req.body;
 
     // const experiences = await Experience.find({user: userId}).exec();
     
     // if (!experiences || experiences.length === 0) {
     //     return res.status(404).json({ message: 'User experiences not found' });
     //     }
+    const experience = {};
     const experienceToUpdate = await Experience.findById(experienceId).exec();
 
     try {
-        // for(const experience of experiences) {
-            if(experience._id.toString() === experienceId){
-                experience.organisation = organisation || experienceToUpdate.organisation;
-                experience.position = position || experienceToUpdate.position;
-                experience.startDate = startDate || experienceToUpdate.startDate;
-                experience.endDate = endDate || experienceToUpdate.endDate;
-                experience.achievements = achievements || experienceToUpdate.achievements;
-                experience.responsibilities = responsibilities || experienceToUpdate.responsibilities;
-                experience.user = user;
-            // }
-            await experience.save();
-        };        
-        res.status(200).json({message: 'Experience successfully updated.'})
-    } catch (error){
+            experience.organisation = organisation || experienceToUpdate.organisation;
+            experience.position = position || experienceToUpdate.position;
+            experience.startDate = startDate || experienceToUpdate.startDate;
+            experience.endDate = endDate || experienceToUpdate.endDate;
+            experience.achievements = achievements || experienceToUpdate.achievements;
+            experience.responsibilities = responsibilities || experienceToUpdate.responsibilities;
+            experience.user = userId;
+            experience._id = experienceToUpdate._id;
+            experience.__v = experienceToUpdate.__v;
+             
+            const updatedExperience = await Experience.findByIdAndUpdate(experienceId, experience, { new: true });
+            res.status(200).json(updatedExperience)
+
+        }  catch (error){
         res.status(500).json({message: 'Error updating experience'})
     }
 }
